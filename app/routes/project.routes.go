80eaff5f7cb49da1d87de489be8518776c25822e
Peder Sandvoll <97838211+pedersandvoll@users.noreply.github.com>
2025-06-12 14:17:54 +0200

    feature/project - project routes

---
+++ app/routes/project.routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/pedersandvoll/project-insight-be/app/handlers"
+	"github.com/pedersandvoll/project-insight-be/config/middleware"
+)
+
+func ProjectRoutes(app *fiber.App, h *handlers.Handlers) {
+	app.Use(cors.New())
+	api := app.Group("/project")
+	api.Use(middleware.AuthRequired(h.JWTSecret))
+
+	api.Get("/", h.GetProjects)
+	api.Post("/create", h.CreateProject)
+}
