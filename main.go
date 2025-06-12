ace2f5cb61a66592780835aae52bdb7512415e99
Peder Sandvoll <97838211+pedersandvoll@users.noreply.github.com>
2025-06-12 10:08:05 +0200

    feature - main.go

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/pedersandvoll/project-insight-be/app/handlers"
+	"github.com/pedersandvoll/project-insight-be/app/routes"
+	"github.com/pedersandvoll/project-insight-be/config/database"
+	"github.com/pedersandvoll/project-insight-be/config/tables"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	dbConfig := database.NewConfig()
+	db, err := database.NewDatabase(dbConfig)
+	if err != nil {
+		log.Fatalf("Could not initialize database: %v", err)
+	}
+
+	tables.RunMigrations(db.DB)
+
+	app := fiber.New()
+
+	h := handlers.NewHandlers(db, dbConfig.JWTSecret)
+
+	routes.AuthRoutes(app, h)
+	routes.Routes(app, h)
+
+	app.Listen(":3000")
+}
